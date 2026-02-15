af3788ab2160e58494417629f15eed35c980a5d7
SolomonDavid Akesobia <sakesobia@gmail.com>
2026-02-15 21:11:33 +0100

    fixed emailQueue

--- backend/queues/emailQueue.js
+++ backend/queues/emailQueue.js
@@ -2,10 +2,6 @@
 import { Queue } from "bullmq";
 import IORedis from "ioredis";
 
-const connection = new IORedis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: process.env.REDIS_PORT || 6379,
-  password: process.env.REDIS_PASSWORD || undefined,
-});
+const connection = new IORedis(process.env.REDIS_HOST);
 
 export const emailQueue = new Queue("emailQueue", { connection });
